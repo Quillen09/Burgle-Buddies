3171308965f58e91c75c24b0857d4581de04ce31
Alexander Murray <alexander.murray@bsu.edu>
2025-03-12 22:13:27 -0400

    Update floorplan tilemap

--- BurgleBuddies/Scripts/player.gd
+++ BurgleBuddies/Scripts/player.gd
@@ -9,6 +9,6 @@ func read_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 		
-func _physics_process(delta):
+func _physics_process(_delta):
 	read_input()
 	move_and_slide()
