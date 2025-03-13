c146f51539213278e3ffe587cbc3cb9abbb9d4d4
Krista Clanton <kristaclanton0@gmail.com>
2025-03-13 04:30:37 -0400

    add grid movement, a knife, and small edits to the layout

--- BurgleBuddies/Scripts/player.gd
+++ BurgleBuddies/Scripts/player.gd
@@ -1,14 +1,28 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var move_distance := 50
 
 var direction : Vector2 = Vector2()
-var charVelocity : Vector2 = Vector2()
+#var charVelocity : Vector2 = Vector2()
 
-func read_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+#func read_input():
+	#var input_direction = Input.get_vector("left", "right", "up", "down")
+	#velocity = input_direction * speed
 		
 func _physics_process(_delta):
-	read_input()
+	#read_input()
+	
+	if Input.is_action_just_pressed("right"):
+		position.x += move_distance
+	if Input.is_action_just_pressed("left"):
+		position.x -= move_distance
+	if Input.is_action_just_pressed("up"):
+		position.y -= move_distance
+	if Input.is_action_just_pressed("down"):
+		position.y += move_distance
+		
+	position.x = clamp(position.x, -430, 2414)
+	position.y = clamp(position.y, -1098, 266)
+		
 	move_and_slide()
