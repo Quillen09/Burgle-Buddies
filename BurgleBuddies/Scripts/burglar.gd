67dc4a0716224084d77e50c2c3ddb5682471c4ca
Krista Clanton <kristaclanton0@gmail.com>
2025-04-03 06:21:42 -0400

    add attack and attack animation

--- BurgleBuddies/Scripts/burglar.gd
+++ BurgleBuddies/Scripts/burglar.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 @export var move_distance := 50
 @onready var itemHeld = "";
 @onready var weaponHeld = "Knife";
+@export var attacking = false
+@onready var animation = $AnimationPlayer
+
 
 var direction : Vector2 = Vector2()
 
@@ -23,3 +26,17 @@ func _physics_process(_delta):
 	position.y = clamp(position.y, -1582, 725)
 		
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("burg_attack"):
+		attack()
+
+func attack():
+	var overlapping_object = $attackArea.get_overlapping_areas()
+	
+	for area in overlapping_object:
+		var parent = area.get_parent()
+		print(parent)
+		parent.queue_free()
+	
+	attacking = true
+	animation.play("attackburg")
--- BurgleBuddies/Scripts/resident.gd
+++ BurgleBuddies/Scripts/resident.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 @export var speed = 300
-@export var move_distance := 50
+@export var attacking = false
+@onready var animation = $AnimationPlayer
 
 var direction : Vector2 = Vector2()
 
@@ -20,3 +21,17 @@ func _physics_process(_delta):
 	position.y = clamp(position.y, -1582, 725)
 		
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("res_attack"):
+		attack()
+
+func attack():
+	var overlapping_object = $attackArea.get_overlapping_areas()
+	
+	for area in overlapping_object:
+		var parent = area.get_parent()
+		print(parent)
+		parent.queue_free()
+	
+	attacking = true
+	animation.play("attack")
