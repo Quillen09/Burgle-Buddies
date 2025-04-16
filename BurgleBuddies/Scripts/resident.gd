7cead019032389d9dc9bb48621720f3e5e1816f8
Alexander Murray <alexander.murray@bsu.edu>
2025-04-16 11:18:15 -0400

    Add death end screen on burglar

--- BurgleBuddies/Scripts/resident.gd
+++ BurgleBuddies/Scripts/resident.gd
@@ -47,6 +47,8 @@ func attack():
 			var parent = area.get_parent()
 			print(parent)
 			parent.queue_free()
+			await get_tree().create_timer(1.0).timeout
+			get_tree().change_scene_to_file("res://Scenes/end.tscn")
 	
 		attacking = true
 		animation.play("attack")
